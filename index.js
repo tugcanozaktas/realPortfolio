7f6e39b3723dad6a9223295749d84e39c19dc595
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-04-28 12:19:08 +0300

    fixed some issues with the code

--- index.js
+++ index.js
@@ -4,24 +4,29 @@ const educationSection = document.getElementById("education-section");
 const experienceIcon = document.getElementById("experience")
 const experienceSection = document.getElementById("experience-section")
 
+const aboutSection = document.getElementById("about")
+
 experienceIcon.addEventListener("click", function(){
-    showFunc(experienceIcon,experienceSection, educationIcon, educationSection)
+    showFunc(aboutSection, experienceIcon,experienceSection, educationIcon, educationSection)
 } , false)
 educationIcon.addEventListener("click", function(){
-    showFunc(educationIcon,educationSection, experienceIcon, experienceSection)
+    showFunc(aboutSection, educationIcon,educationSection, experienceIcon, experienceSection)
 } , false);
 
 
 
-function showFunc(icon,section, otherIcon, otherSection){
+function showFunc(cover, icon,section, otherIcon, otherSection){
+    console.log("hello")
     if(window.getComputedStyle(section).opacity !== "0"){
         icon.style.padding = 0;
         icon.style.backgroundColor = "transparent";
         section.style.opacity = "0";
+        
     } else if(window.getComputedStyle(section).opacity == "0" && window.getComputedStyle(otherSection).opacity == "0"){
         icon.style.padding = "10px";
         icon.style.backgroundColor = "rgb(10, 9, 9)"
         section.style.opacity = "1";
+        
     } else if(window.getComputedStyle(section).opacity == "0" && window.getComputedStyle(otherSection).opacity !== "0"){
 
         otherIcon.style.padding = 0;
